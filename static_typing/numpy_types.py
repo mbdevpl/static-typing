c6fdddf0a4e6334524c96a87d0a9fe7583f2e7ee
Mateusz Bysiek <mb@mbdev.pl>
2017-09-18 22:56:58 +0900

    testing ndarray wrapper

--- static_typing/numpy_types.py
+++ static_typing/numpy_types.py
@@ -4,36 +4,36 @@ import typing as t
 
 import numpy as np
 
-def create_typed_numpy_ndarray(dims: int, dtype: t.ClassVar):
+def create_typed_numpy_ndarray(dims: int, data_type: t.ClassVar):
 
-    class typed_ndarray(np.ndarray):
+    def typed_ndarray(*args, **kwargs):
+        """Create an instance of numpy.ndarray which must conform to declared type constraints."""
 
-        """Statically typed version of numpy.ndarray class."""
+        shape_loc = (args, 0) if len(args) > 0 else (kwargs, 'shape')
+        dtype_loc = (args, 1) if len(args) > 1 else (kwargs, 'dtype')
 
-        _dims = dims
+        shape = shape_loc[0][shape_loc[1]]
+        if shape is not None and (dims != 1 if isinstance(shape, int) else len(shape) != dims):
+            raise ValueError(f'actual ndarray shape {shape} conflicts'
+                            f' with its declared dimensionality of {dims}')
 
-        _dtype = dtype
+        try:
+            dtype = dtype_loc[0][dtype_loc[1]]
+        except KeyError:
+            dtype = None
+        if dtype is not None and dtype is not data_type:
+            raise TypeError(f'actual ndarray dtype {dtype} conflicts'
+                            f' with its declared dtype {data_type}')
+        dtype_loc[0][dtype_loc[1]] = data_type
 
-        def __init__(self, *args, **kwargs):
-
-            shape = args[0] if len(args) > 0 else kwargs.get('shape', None)
-            if shape is not None and len(shape) != self._dims:
-                raise TypeError(f'actual ndarray shape {shape} conflicts'
-                                f' with its declared dimensionality of {self._dims}')
-
-            dtype = kwargs.get('dtype', None)
-            if dtype is not None and dtype is not self._dtype:
-                raise TypeError(f'actual ndarray dtype {dtype} conflicts'
-                                f' with its declared dtype {self._dtype}')
-            kwargs['dtype'] = self._dtype
-
-            super().__init__(*args, **kwargs)
+        #print('np.ndarray', args, kwargs)
+        return np.ndarray(*args, **kwargs)
 
     return typed_ndarray
 
 ndarray = {
-    (dims, dtype): create_typed_numpy_ndarray(dims, type)
-    for dtype in [int, np.int8, np.int16, np.int32, np.int64,
+    (dims, data_type): create_typed_numpy_ndarray(dims, data_type)
+    for data_type in [int, np.int8, np.int16, np.int32, np.int64,
                   float, np.float16, np.float32, np.float64,
                   bool, np.bool]
     for dims in [1, 2, 3]}
---
+++ test/test_types.py
@@ -0,0 +1,40 @@
+"""Tests for statically declared types for various objects."""
+
+import unittest
+
+import numpy as np
+
+from static_typing.numpy_types import ndarray
+
+class Tests(unittest.TestCase):
+
+    maxDiff = None
+
+    def test_numpy_1d_ndarray(self):
+        for data_type in (bool, int, float):
+                with self.subTest(data_type=data_type):
+                    ordinary = np.ndarray(10, dtype=data_type)
+                    statically_typed = ndarray[1, data_type](10)
+                    self.assertEqual(ordinary.shape, statically_typed.shape)
+                    self.assertEqual(ordinary.dtype, statically_typed.dtype)
+                    with self.assertRaises(ValueError):
+                        ndarray[1, data_type]((10,10))
+                    with self.assertRaises(TypeError):
+                        ndarray[1, data_type](10, dtype=object)
+
+    def test_numpy_general_ndarray(self):
+        for data_type in (bool, int, float):
+            for dimensionality in (1, 2, 3):
+                with self.subTest(data_type=data_type, dimensionality=dimensionality):
+                    shape = tuple([10 for _ in range(dimensionality)])
+                    ordinary = np.ndarray(shape, dtype=data_type)
+                    statically_typed_a = ndarray[dimensionality, data_type](shape)
+                    self.assertEqual(ordinary.shape, statically_typed_a.shape)
+                    self.assertEqual(ordinary.dtype, statically_typed_a.dtype)
+                    statically_typed_b = ndarray[dimensionality, data_type](shape, dtype=data_type)
+                    self.assertEqual(ordinary.shape, statically_typed_b.shape)
+                    self.assertEqual(ordinary.dtype, statically_typed_b.dtype)
+                    with self.assertRaises(ValueError):
+                        ndarray[dimensionality, data_type](shape + (10,))
+                    with self.assertRaises(TypeError):
+                        ndarray[dimensionality, data_type](shape, dtype=object)
