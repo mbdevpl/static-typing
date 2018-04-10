496bdeacae9e316263054dde584fb718a523d350
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-04-10 17:11:23 +0900

    restoring test coverage

--- test/test_dump_unparse.py
+++ test/test_dump_unparse.py
@@ -15,16 +15,15 @@ _LOG = logging.getLogger(__name__)
 
 class Tests(unittest.TestCase):
 
-    def _validate_dump_unparse(self, tree, ast_module, *args, **kwargs):
-        with self.subTest(*args, ast_module=ast_module, **kwargs):
-            tree_dump = dump(tree)
-            self.assertIsInstance(tree_dump, str)
-            code = unparse(tree)
-            self.assertIsInstance(code, str)
-            try:
-                ast_module.parse(code)
-            except SyntaxError:
-                _LOG.warning('%s failed to parse unparsed code """%s"""', ast_module, code)
+    def _validate_dump_unparse(self, tree, ast_module):
+        tree_dump = dump(tree)
+        self.assertIsInstance(tree_dump, str)
+        code = unparse(tree)
+        self.assertIsInstance(code, str)
+        try:
+            ast_module.parse(code)
+        except SyntaxError:
+            raise ValueError('{} failed to parse unparsed code """{}"""'.format(ast_module, code))
 
     def test_resolve(self):
         for ast_module, eval_ in itertools.product(AST_MODULES, (False, True)):
@@ -32,8 +31,8 @@ class Tests(unittest.TestCase):
             for description, example in SOURCE_CODES.items():
                 tree = ast_module.parse(example)
                 tree = resolver.visit(tree)
-                self._validate_dump_unparse(
-                    tree, ast_module, description=description, example=example)
+                with self.subTest(ast_module=ast_module, description=description, example=example):
+                    self._validate_dump_unparse(tree, ast_module)
 
     def test_statically_type(self):
         for ast_module in AST_MODULES:
@@ -43,8 +42,8 @@ class Tests(unittest.TestCase):
                 tree = ast_module.parse(example)
                 tree = resolver.visit(tree)
                 tree = typer.visit(tree)
-                self._validate_dump_unparse(
-                    tree, ast_module, description=description, example=example)
+                with self.subTest(ast_module=ast_module, description=description, example=example):
+                    self._validate_dump_unparse(tree, ast_module)
 
     def test_augment(self):
         for ast_module in AST_MODULES:
@@ -52,13 +51,20 @@ class Tests(unittest.TestCase):
                 tree = ast_module.parse(example)
                 tree = augment(tree, globals_=GLOBALS_EXTERNAL, locals_=LOCALS_NONE,
                                ast_module=ast_module)
-                self._validate_dump_unparse(
-                    tree, ast_module, description=description, example=example)
+                with self.subTest(ast_module=ast_module, description=description, example=example):
+                    self._validate_dump_unparse(tree, ast_module)
 
     def test_parse(self):
         for ast_module in AST_MODULES:
             for description, example in SOURCE_CODES.items():
                 tree = parse(example, globals_=GLOBALS_EXTERNAL, locals_=LOCALS_NONE,
                              ast_module=ast_module)
-                self._validate_dump_unparse(
-                    tree, ast_module, description=description, example=example)
+                with self.subTest(ast_module=ast_module, description=description, example=example):
+                    self._validate_dump_unparse(tree, ast_module)
+
+    def test_invalid(self):
+        for ast_module in AST_MODULES:
+            tree = ast_module.If(ast_module.Pass(), [ast_module.Pass()], [])
+            with self.subTest(ast_module=ast_module):
+                with self.assertRaises(ValueError):
+                    self._validate_dump_unparse(tree, ast_module)
