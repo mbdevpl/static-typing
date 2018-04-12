fc1be1702ffc21d0d830816334c0ba542fddb791
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-04-12 17:30:47 +0900

    avoid resolving type hints to built-in ast unless necessary
    Signed-off-by: Mateusz Bysiek <mateusz.bysiek@gmail.com>

--- static_typing/augment.py
+++ static_typing/augment.py
@@ -14,8 +14,9 @@ _LOG = logging.getLogger(__name__)
 def augment(tree, eval_: bool = True, globals_=None, locals_=None, ast_module=typed_ast.ast3):
     """Add static type information to the given AST."""
 
-    type_hint_resolver = TypeHintResolver[ast_module, ast](eval_=eval_, globals_=globals_,
-                                                           locals_=locals_)
+    parser_ast_module = ast if eval_ else ast_module
+    type_hint_resolver = TypeHintResolver[ast_module, parser_ast_module](
+        eval_=eval_, globals_=globals_, locals_=locals_)
     tree = type_hint_resolver.visit(tree)
     _LOG.debug('%s', ast_module.dump(tree))
 
